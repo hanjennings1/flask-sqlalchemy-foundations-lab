b3a20220ea712a904f552f75ee45ffbabd01f581
Hannah Jennings <han.jennings1@gmail.com>
2026-09-22 17:04:12 -0400

    Add Earthquake model, migrations, and earthquake-by-id route

--- server/app.py
+++ server/app.py
@@ -20,7 +20,21 @@ def index():
     body = {'message': 'Flask SQLAlchemy Lab 1'}
     return make_response(body, 200)
 
-# Add views here
+
+# GET EARTHQUAKE BY ID:
+@app.route('/earthquakes/<int:id>')  
+def earthquake_by_id(id):
+    quake = Earthquake.query.filter(Earthquake.id == id).first()
+
+    if quake:
+        body = quake.to_dict()
+        status = 200
+    else:
+        # error handling / error message:
+        body = {"message": f"Earthquake {id} not found."}
+        status = 404
+
+    return make_response(body, status)
 
 
 if __name__ == '__main__':
--- server/models.py
+++ server/models.py
@@ -5,4 +5,21 @@ metadata = MetaData()
 
 db = SQLAlchemy(metadata=metadata)
 
-# Add models here
+class Earthquake(db.Model):
+    __tablename__ = "earthquakes"     # the table's name in the database
+
+    id = db.Column(db.Integer, primary_key=True)    # Unique ID will be assigned automatically
+    magnitude = db.Column(db.Float)                 # Float = decimal number
+    location = db.Column(db.String)   
+    year = db.Column(db.Integer)
+
+    def to_dict(self):  # converts an Earthquake into a dictionary
+        return {
+            "id": self.id,
+            "magnitude": self.magnitude,
+            "location": self.location,
+            "year": self.year,
+        }
+
+    def __repr__(self):     # controls how an Earthquake will print
+        return f"<Earthquake {self.id}, {self.magnitude}, {self.location}, {self.year}>"
