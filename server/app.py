f58ed3a6ad3905ff129e1af307c0932639362c0a
Hannah Jennings <han.jennings1@gmail.com>
2026-09-22 17:11:33 -0400

    Add earthquakes-by-magnitude route

--- server/app.py
+++ server/app.py
@@ -21,7 +21,7 @@ def index():
     return make_response(body, 200)
 
 
-# GET EARTHQUAKE BY ID:
+# GET EARTHQUAKE BY ID ---
 @app.route('/earthquakes/<int:id>')  
 def earthquake_by_id(id):
     quake = Earthquake.query.filter(Earthquake.id == id).first()
@@ -35,6 +35,20 @@ def earthquake_by_id(id):
         status = 404
 
     return make_response(body, status)
+
+
+# GET EARTHQUAKES MATCHING A MIN MAGNITUDE VALUE ---
+@app.route('/earthquakes/magnitude/<float:magnitude>')  # <float:...> needs a decimal in the URL
+def earthquakes_by_magnitude(magnitude):
+    quakes = Earthquake.query.filter(Earthquake.magnitude >= magnitude).all()
+
+    body = {
+        "count": len(quakes),                               # displays number of matches
+        "quakes": [quake.to_dict() for quake in quakes],    # shows each match as a dictionary with its info
+    }
+
+    return make_response(body, 200) # always returns 200 -- even if none match, the request still works, just returns none
+
 
 
 if __name__ == '__main__':
